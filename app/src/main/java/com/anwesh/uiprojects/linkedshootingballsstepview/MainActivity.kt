3e355706b648ead5abb819b56f46fc9e31dd10fb
Anwesh43 <anweshthecool0@gmail.com>
2019-01-05 12:51:09 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedshootingballsstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedshootingballsstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedshootingballsstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.shootingballstepview.ShootingBallStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ShootingBallStepView.create(this)
     }
 }
