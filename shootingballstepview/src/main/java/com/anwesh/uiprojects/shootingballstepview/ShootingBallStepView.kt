616d57b69412a7ad06a0b42673993349f942a3f5
Anwesh43 <anweshthecool0@gmail.com>
2019-01-05 11:54:01 +0530

    adding librarues and defining constants

--- shootingballstepview/src/main/java/com/anwesh/uiprojects/shootingballstepview/ShootingBallStepView.kt
+++ shootingballstepview/src/main/java/com/anwesh/uiprojects/shootingballstepview/ShootingBallStepView.kt
@@ -3,3 +3,23 @@ package com.anwesh.uiprojects.shootingballstepview
 /**
  * Created by anweshmishra on 05/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val balls : Int = 4
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+val hSizeFactor : Float = 4.5f
+val wSizeFactor : Float = 3.2f
+val stroeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
